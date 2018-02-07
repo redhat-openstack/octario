615472e2a0035b2c8139613afd9f20ae20897429
Sorin Sbarnea <ssbarnea@redhat.com>
2018-02-07 14:02:03 +0000

    improve console output
    Protects console from getting too much output
    while executing tests. Full output would be
    saved into text file and only a tail will be
    displayed to console. Full log is also collected.
    
    When running under Jenkins the last message
    contains a clickable URL towards the the full
    captured output.
    
    Avoids hammering CI and creating a potential
    outage due to known inifinite-console-pipeline bug.
    https://issues.jenkins-ci.org/browse/JENKINS-44483
    
    Also mentions shade as a requirement, which is needed
    for running tests (ansible does not install it).
    
    Change-Id: I3aff75c530a04b8f1448957a150a1f482b267b0c

--- runtests.sh
+++ runtests.sh
@@ -86,7 +86,11 @@ function finish {
   if [ "${DISABLE_CLEANUP:-false}" != "true" ]; then
       set -e
       pushd $IR_DIR >>/dev/null
-      infrared openstack ${IR_CLOUD:-} --image='fake_image' --prefix=$PREFIX --cleanup=yes > ${DIR}/cleanup.log
+      infrared openstack ${IR_CLOUD:-} --image='fake_image' --prefix=$PREFIX --cleanup=yes > ${DIR}/cleanup.log || {
+        >&2 cat ${DIR}/cleanup.log
+        echo -e "WARN: Cleanup failure."
+        rv=99
+      }
       infrared workspace delete ${PREFIX}
   fi
 
@@ -101,7 +105,7 @@ pushd $IR_DIR >>/dev/null
   echo -e "INFO: Using prefix ${HIGHLIGHT}${PREFIX}${NORMAL} and running from ${HIGHLIGHT}${IR_DIR}${NORMAL} ..."
 
   # we create unique workspace to avoid reusing existing one
-  infrared workspace checkout -c ${PREFIX}
+  infrared workspace checkout ${PREFIX} || true
   infrared workspace node-list
 
   # avoid provisioning in case octario is deeply broken
